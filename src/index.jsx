21d6cf20f1d7ffd72c14c39fc0b3f5f01a4651d0
August Voelker <gusvoelker@outlook.com>
2022-09-26 13:47:58 -0400

    initial folder setup

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+const App = () => {
+  return <h1>Hello World</h1>
+}
+
+root.render(<App />);
