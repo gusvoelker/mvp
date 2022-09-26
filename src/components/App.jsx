2b51eccf658aca87c07f2be545a11e1add6500ca
August Voelker <gusvoelker@outlook.com>
2022-09-26 15:19:12 -0400

    initial calendar setup

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import Calendar from './Calendar/Calendar.jsx';
+const App = () => {
+
+  const style = {
+    position: "relative",
+    margin: "auto"
+  }
+
+  return (
+    <>
+      <h1>Dinner Time</h1>
+      <Calendar style={style} width="302px"/>
+    </>
+  )
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
+import React from 'react';
 import { createRoot } from "react-dom/client";
+import App from './components/App.jsx';
 const root = createRoot(document.getElementById("root"));
 
-const App = () => {
-  return <h1>Hello World</h1>
-}
+
 
 root.render(<App />);
