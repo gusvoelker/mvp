02d6716e6cb940e0844f70cd89078e706c895091
August Voelker <gusvoelker@outlook.com>
2022-09-27 13:46:13 -0400

    added add meal button

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -2,14 +2,16 @@ import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import RadioButtonsGroup from './Radio.jsx';
 import Filters from './Filters.jsx';
+import Button from '@mui/material/Button';
+
 
 const OptContainer = styled.div`
-  height: 482px;
+  height: 452px;
   width: 20%;
   position: absolute;
   margin: 5% 0 0 5%;
   text-align: center;
-  padding-top: 10px;
+  padding-top: 40px;
   font-size: 1.4em;
   border: 2px solid grey;
   `
@@ -18,6 +20,9 @@ const Form = styled.form`
   margin-top: 6%;
 `
 const FilterTitle = styled.div`
+margin-top: 10%;
+`
+const BttnContainer = styled.div`
 margin-top: 10%;
 `
 
@@ -29,6 +34,9 @@ const Options = () => {
         <RadioButtonsGroup />
         <FilterTitle>Random Options</FilterTitle>
         <Filters />
+        <BttnContainer>
+          <Button variant="contained" sx={{fontSize: 24, width: '90%'}}> Add Meal </Button>
+        </BttnContainer>
       </Form>
     </OptContainer>
   )
