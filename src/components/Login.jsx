04d2b128ab09b01d7b5c440601c0e770379b0816
August Voelker <gusvoelker@outlook.com>
2022-11-27 22:49:42 -0500

    add google login button and styling changes to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import GoogleButton from 'react-google-button';
+
 
 
 
@@ -9,12 +11,13 @@ export default function (props) {
     e.preventDefault();
     props.setPage('home')
   }
+
   const PageContainer = styled.div`
     background-color: white;
     height: 100vh;
     width: 100vw;
     display: flex;
-    `
+  `
 
   const Image = styled.div`
       background-color: #f2d184;
@@ -22,12 +25,13 @@ export default function (props) {
       height: 100%;
       display: inline-flex;
       flex-direction: column;
-    `
+  `
 
   const Login = styled.div`
     background-color: white;
-    display: inline-block;
-    width: 60%;
+    display: inline-flex;
+    flex-direction: column;
+    width: 64%;
   `
 
   const Logo = styled.img`
@@ -54,22 +58,36 @@ export default function (props) {
   max-width: 60%;
   `
 
-  const SwitchToSignIn = styled.p`
-    text-align: right;
+  const Nav = styled.nav`
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 30px 30px 0;
   `
+
+  const LoginMain = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  `
+
   const LoginHeading = styled.h1`
-    text-align: center;
+
   `
 
   const LoginLineBreak = styled.hr`
     & {
-      width: 60%;
+      width: 100%;
       border: none;
       border-top: 2px solid #e0e0e0;
       color: #6e6d7a;
       overflow: visible;
       text-align: center;
       height: 5px;
+      margin-top: 40px;
     }
     &:after{
       background: #fff;
@@ -80,27 +98,50 @@ export default function (props) {
     }
   `
 
-  const LoginInputs = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-    width: 100%;
+  const LoginContent=styled.div`
+    padding: 30px 60px 0;
+    width: 50%;
   `
 
   const Label = styled.label`
     display: block;
-    width: 60%;
+    width: 100%;
+    margin-bottom: 30px;
   `
+
+  const PairedLabel = styled.label`
+    width: 47.5%;
+    margin-top: 10px;
+    margin-bottom: 30px;
+  `
+
+  const PairedInputContainer = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  `
+
 
   const TextInput = styled.input`
   width: 100%;
-  font-size: 1.4em;
-  padding: 5px;
+  font-size: 1em;
+  padding: 10px 16px;
   box-sizing: border-box;
   -moz-box-sizing: border-box;
   display: block;
+  border: 1px solid transparent;
+  border-radius: 8px;
+  height: 40px;
+  padding: 10px 16px;
+  background-color: #f3f3f4;
+  color: #0d0c22;
+  outline: none;
+  &:hover, &:focus {
+    box-shadow: 0 0 0 4px rgb(234 76 137 / 10%);
+    background-color: white;
+    border-color: rgba(234,76,137,0.4);
+  }
+  transition: background-color 200ms ease, outline 200ms ease, color 200ms ease, box-shadow 200ms ease, -webkit-box-shadow 200ms ease;
   `
 
   const LoginSubmit = styled.button`
@@ -119,22 +160,41 @@ export default function (props) {
         </LogoContainer>
       </Image>
       <Login>
-        <SwitchToSignIn>Already have an account? <a href="" onClick={handleHome}>Sign In</a></SwitchToSignIn>
-        <LoginHeading>
-        Sign up to Dinner Time
-        </LoginHeading>
-        <LoginLineBreak/>
-        <LoginInputs>
-          <Label>
-            Username
-            <TextInput type="text"></TextInput>
-          </Label>
-          <Label>
-            Password
-            <TextInput type="text"></TextInput>
-          </Label>
-          <LoginSubmit>submit</LoginSubmit>
-        </LoginInputs>
+        <Nav>
+          <p>Already have an account? <a href="" onClick={handleHome}>Sign In</a></p>
+        </Nav>
+        <LoginMain>
+          <LoginContent>
+            <LoginHeading>
+            Sign up to Dinner Time
+            </LoginHeading>
+            <GoogleButton type="dark"/>
+            <LoginLineBreak/>
+              <PairedInputContainer>
+                <PairedLabel>
+                  Name
+                  <TextInput type="text" autocomplete="name"></TextInput>
+                </PairedLabel>
+                <PairedLabel>
+                  Username
+                  <TextInput type="text"></TextInput>
+                </PairedLabel>
+              </PairedInputContainer>
+              <Label>
+                Email
+                <TextInput type="text"></TextInput>
+              </Label>
+              <Label>
+                Password
+                <TextInput type="text"></TextInput>
+              </Label>
+              <Label>
+                Confirm Password
+                <TextInput type="text"></TextInput>
+              </Label>
+              <LoginSubmit>submit</LoginSubmit>
+          </LoginContent>
+        </LoginMain>
       </Login>
     </PageContainer>
   )
